99025dc742c1079eedadf09727d9c77750a079e0
徐志强 <zhiqiang.xu@dolphindb.com>
2023-10-03 21:41:09 +0200

    rename gh-pages default html

--- build/ebiten_game/wasm/deploy_gh-page.sh
+++ build/ebiten_game/wasm/deploy_gh-page.sh
@@ -9,14 +9,14 @@ sh build_wasm.sh; # build wasm and move to this dir
 rm -rf gh-pages && mkdir gh-pages;
 
 
-cp ebiten_game.html ebiten_game.wasm wasm_exec.js gh-pages/
+cp index.html ebiten_game.wasm wasm_exec.js gh-pages/
 
 cd gh-pages/;
 
 git init
 git remote add origin git@github.com:zq-xu/2d-game.git
 
-git add ebiten_game.html ebiten_game.wasm wasm_exec.js
+git add .
 
 git commit -m "auto deploy ${commitId}"
 
